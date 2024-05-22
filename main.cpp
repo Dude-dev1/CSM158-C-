af24cb66ec8e8a3aa3322a10e5d238ca103969a0
Stephen Boakye-Yiadom <syiadom962@gmail.com>
2024-05-22 00:39:47 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int factor;
+    cout << "Enter the value of the factor here ";
+    cin >> factor;
+
+    cout << ( factor == 2 ? "The number is prime" : "The number is not prime");
+
+    return 0;
+}
